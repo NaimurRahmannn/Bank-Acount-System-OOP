f307b121ab413951ad3c7fb1b34356d4f2594068
NaimurRahmann <niamurreiman@gmail.com>
2026-06-10 16:25:33 +0600

    feat: Encapsulate the user account balance

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ class Bank:
     def __init__(self, account_number, owner, balance=0):
         self.account_number = account_number
         self.owner = owner
-        self.balance = balance
+        self.__balance = balance
         self.transactions = []
 
     def deposite(self,amount):
         if amount<=0:
             print("Deposit amount must be positive")
             return
-        self.balance+=amount
+        self.__balance+=amount
     
     def withdraw(self,amount):
-        if self.balance-amount<0:
+        if self.__balance-amount<0:
            print("Balance insufficient")
            return
-        self.balance-=amount
+        self.__balance-=amount
 
     def check_balance(self):
-        return self.balance  
+        return self.__balance  
 
 account1=Bank("Naimur","Rahman",1000)
 account1.deposite(500)
 account1.withdraw(500)
-print(account1.balance)
+#print(account1.balance)
 print(account1.check_balance())
